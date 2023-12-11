63e6e86fe841e0f040cb1df2dc7ab563fd28ecee
apokryphos <luca.sandrock@t-online.de>
2023-12-11 20:29:44 +0100

    feat: 404 page typewriter -> dialog

--- src/routes/404.tsx
+++ src/routes/404.tsx
@@ -1,11 +1,13 @@
-import Typewriter from "../components/Typewriter";
+import { Dialog } from "../components/Dialog";
 
 export default function _404Page() {
     return (
-        <div className='w-[100vw] h-[100vh] flex items-center justify-center'>
-            <h1 className='text-4xl font-bold'>
-                <Typewriter letterDelay={60} text='404... How did you even get here?' />
-            </h1>
+        <div className='w-[100vw] h-[100vh] flex items-center justify-center text-4xl font-bold'>
+            <Dialog instructions={[
+                { text: "404", timeout: 1000, letterDelay: 100 },
+                { text: "...", letterDelay: 400 },
+                { text: "How did u even get here?", timeout: 2000, letterDelay: 50 },
+            ]} />
         </div>
     );
 }
