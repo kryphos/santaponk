127e1edffb71853d40fc5bcec0cd90de936b78bb
Luca Sandrock <luca.sandrock@t-online.de>
2023-12-23 20:55:54 +0100

    fix: dialog fixes

--- src/data.ts
+++ src/data.ts
@@ -13,7 +13,7 @@ export const DATA: Data = {
     doag: {
         name: "Doag",
         message: collapse_whitespace(`
-            Hi Doag! You're such a cuet and smort and talented and funny and cool guy. I had lots of 
+            Hi Doag! You're such a cuet and smort and talented and funny and cool boi. I had lots of 
             fun hanging out with you and your community, both on stream and on discord. I have a little
             gift for you where you can show off your blender skills and put all your links and other stuff,
             a smoll webpage. While it is empty and boring right now, the background is basically
@@ -47,12 +47,13 @@ export const DATA: Data = {
             Hellau Sana! It's been a year since I first was mesmerized by your art. You're undebatably
             one of the coolest people I've found on the internet and that's why I have a little something
             for you. To boost your social media presence, I thought you would like a small personalized 
-            link tree page. But it's not just any link tree page, in fact, its... empty and boring.
-            I couldn't just presume to design this for you. So you can prompt me to add whatever you want.
-            Sounds? Music? Particles? The background a 3D flappy bird clone where the brid is a tiny sana?
-            Your turn. I'll make it happen. One more thing. I peeked at the requirements for the 
-            internship you're applying to while you were streaming it and saw that they want a "link to portfolio". 
-            If you don't have one, I surely could make one for you. Just let me know if u want ^^
+            webpage where you can put links, art and other stuff. But it's not just any link webpage,
+            in fact, its... empty and boring. I couldn't just presume to design this for you. So you
+            can prompt me to add whatever you want. Sounds? Music? Particles? The background a 3D
+            flappy bird clone where the brid is a tiny sana? Your turn. I'll make it happen.
+            One more thing. I peeked at the requirements for the internship you're applying to while
+            you were streaming it and saw that they want a "link to portfolio". If you don't have one,
+            I surely could make one for you. Just let me know if u want ^^
         `),
     },
     smead: {
